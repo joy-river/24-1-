201d1a774151d9415640285b0158862c24000988
Joyriver-laptop <dlrkd1122@gmail.com>
2024-04-09 18:06:39 +0900

    fix

--- 컴퓨터시스템설계 - 서태원/Assignment/03/csd_main.c
+++ 컴퓨터시스템설계 - 서태원/Assignment/03/csd_main.c
@@ -3,14 +3,9 @@
 
 int csd_main(int32_t s)
 {
-	int8_t test = 128;
 
-	for(int i = 0; i < 8; i++){
-		if (s & (test >> i))
-			return usleep((100 + 100 * i) * 1000);
-	}
-
+	if (s <= 8) 
+		return usleep((100 * s) * 1000);
 	return sleep(1);
 
 }
-
