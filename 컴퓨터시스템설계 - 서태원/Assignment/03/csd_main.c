a3ad624654c30feea18864a6f567eecfd72a3910
harigaze <dlrkd1122@gmail.com>
2024-04-10 23:31:48 +0900

    done

--- 컴퓨터시스템설계 - 서태원/Assignment/03/csd_main.c
+++ 컴퓨터시스템설계 - 서태원/Assignment/03/csd_main.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
 #include <unistd.h>
 
-int csd_main(int32_t s)
+int csd_main(int32_t s)					// csd_main은 r0 레지스터에 저장된 가장 높은 스위치 값을 s로 받습니다.
 {
 
-	if (s <= 8) 
-		return usleep((100 * s) * 1000);
-	return sleep(1);
+	if (s <= 8)							// 가장 높은 스위치 값에 따라 딜레이를 결정합니다.
+		return usleep((100 * s) * 1000);// usleep의 단위가 마이크로초이므로 ((100 * s) * 1000)msec만큼 딜레이를 가집니다.
+	return sleep(1);					// 만약 어떤 스위치도 켜지지 않았다면, 1초의 딜레이를 가집니다.
 
 }
