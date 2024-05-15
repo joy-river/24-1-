b3993e556dbf0d801854f798e11120abbc46902d
harigaze <dlrkd1122@gmail.com>
2024-05-15 19:10:12 +0900

    update

--- 데이터베이스 - 정연돈/Assignment/04/app.py
+++ 데이터베이스 - 정연돈/Assignment/04/app.py
@@ -1,5 +1,6 @@
 import psycopg2
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session
+from random import choice
 
 app = Flask(__name__)
 connect = psycopg2.connect("dbname=tutorial user=postgres password=postgres")
@@ -8,32 +9,42 @@ cur = connect.cursor()  # create cursor
 
 def sel_movie(order):
     if not order:
-        order = 'rel_date'
-    cur.execute(
-        f"select title, avg(ratings), director, genre, rel_date from movies left join reviews on id = mid group by title, director, genre, rel_date order by {order} desc")
+        order = 'rel_date desc'
+    if type(order) == int:
+        cur.execute(
+            f"select id, title, avg(ratings), director, genre, rel_date "
+            f"from movies left join reviews on id = mid "
+            f"where id = '{order}' "
+            f"group by id, title, director, genre, rel_date")
+    else:
+        cur.execute(
+            f"select id, title, avg(ratings), director, genre, rel_date "
+            f"from movies left join reviews on id = mid "
+            f"group by id, title, director, genre, rel_date order by {order}")
     return cur.fetchall()
 
 
-def sel_review(order='rev_time'):
-    cur.execute(
-        f"select ratings, uid, title, review text, rev_time from reviews, (select id, title from movies) where id = mid order by {order} desc")
+def sel_review(order):
+    id = session['id']
+    if not order:
+        order = 'rev_time desc'
+    if order == 'follow':
+        cur.execute(
+            f"select ratings, uid, mid, title, review, rev_time "
+            f"from reviews, (select id, title from movies) "
+            f"where id = mid and uid in "
+            f"(select opid from ties where id = '{id}' and tie = 'follow')")
+    else:
+        cur.execute(
+            f"select ratings, uid, mid, title, review, rev_time "
+            f"from reviews, (select id, title from movies) "
+            f"where id = mid order by {order}")
     return cur.fetchall()
 
 
 @app.route('/')
 def main():
     return render_template("main.html")
-
-
-@app.route('/main/<id>', methods=['get', 'post'])
-def mainpage(id):
-
-    mov_order = request.form.get('mov_order')
-
-    movies = sel_movie(mov_order)
-    reviews = sel_review()
-
-    return render_template('mainpage.html', id=id, movies=movies, reviews=reviews)
 
 
 @app.route('/register', methods=['get', 'post'])
@@ -65,8 +76,194 @@ def register():
         elif result[0][1] != password:
             flash('비밀번호가 틀립니다. 다시 입력해주세요.', 'error')
         else:
-            return redirect(url_for('mainpage', id=id))
+            session['id'] = id
+            return redirect(url_for('main_page'))
     return render_template('main.html')
+
+
+@app.route('/main', methods=['get', 'post'])
+def main_page():
+    id = session['id']
+    mov_order = request.form.get('mov_order')
+    if mov_order:
+        session['mov_order'] = mov_order
+    else:
+        mov_order = session.get('mov_order', 'rel_date desc')
+
+    rev_order = request.form.get('rev_order')
+    if rev_order:
+        session['rev_order'] = rev_order
+    else:
+        rev_order = session.get('rev_order', 'rev_time desc')
+
+    movies = sel_movie(mov_order)
+    reviews = sel_review(rev_order)
+
+    return render_template('main_page.html', id=id, movies=movies, reviews=reviews)
+
+
+@app.route('/movie/<mid>', methods=['get', 'post'])
+def movie_info(mid):
+    id = session['id']
+    movie = sel_movie(int(mid))[0]
+
+    cur.execute(
+        f"select ratings, uid, review text, rev_time "
+        f"from reviews "
+        f"where mid = '{mid}' and uid not in "
+        f"(select opid from ties where id = '{id}' and tie = 'mute') ")
+    reviews = cur.fetchall()
+    back = request.form.get('back')
+
+    return render_template('movie_info.html', id=id, movie=movie, reviews=reviews)
+
+
+@app.route('/update-review/<mid>', methods=['post'])
+def update_review(mid):
+    id = session['id']
+
+    mid = int(mid)
+    ratings = request.form.get('rating')
+    review = request.form.get('review')
+
+    cur.execute(f"select uid from reviews where uid = '{id}' and mid = '{mid}'")
+    result = cur.fetchone()
+    if result:
+        cur.execute(
+            f"update reviews set ratings = '{ratings}', review = '{review}' "
+            f"where uid = '{id}' and mid = '{mid}'")
+    else:
+        cur.execute(f"insert into reviews values "
+                    f"('{mid}', '{id}', '{ratings}', '{review}', now())")
+
+    connect.commit()
+
+    return redirect(url_for('movie_info', mid=mid))
+
+
+@app.route('/user/<uid>', methods=['get', 'post'])
+def user_info(uid):
+    id = session['id']
+
+    cur.execute(f"select id, role "
+                f"from users "
+                f"where id = '{uid}'")
+    user = cur.fetchone()
+
+    cur.execute(
+        f"select ratings, mid, title, review, rev_time "
+        f"from reviews, (select id, title from movies) "
+        f"where uid = '{uid}' and id = mid")
+    reviews = cur.fetchall()
+
+    cur.execute(f"select id "
+                f"from ties "
+                f"where opid = '{uid}' and tie='follow'")
+    followers = cur.fetchall()
+
+    if id == uid:
+        cur.execute(f"select opid "
+                    f"from ties "
+                    f"where id = '{uid}' and tie='follow'")
+        followed = cur.fetchall()
+        cur.execute(f"select opid "
+                    f"from ties "
+                    f"where id = '{uid}' and tie='mute'")
+        muted = cur.fetchall()
+        cur.execute(f"select id, title "
+                    f"from movies")
+        movies = cur.fetchall()
+
+        return render_template('user_info.html',
+                               id=id, user=user, reviews=reviews,
+                               followers=followers,
+                               followed=followed,
+                               muted=muted, movies=movies)
+
+    return render_template('user_info.html', id=id, user=user,
+                           reviews=reviews, followers=followers)
+
+
+@app.route('/follow-mute/<uid>', methods=['post'])
+def follow_mute(uid):
+    id = session['id']
+    act = request.form.get('act')
+    back = request.form.get('back')
+
+    if act == 'follow' or act == 'unmute':
+        cur.execute(f"delete "
+                    f"from ties "
+                    f"where id = '{id}' and opid = '{uid}' and tie = 'mute'")
+
+    if act == 'mute' or act == 'unfollow':
+        cur.execute(f"delete "
+                    f"from ties "
+                    f"where id = '{id}' and opid = '{uid}' and tie = 'follow'")
+
+    if act == 'follow' or act == 'mute':
+        cur.execute(f"select * "
+                    f"from ties "
+                    f"where id = '{id}' and opid='{uid}' and tie = '{act}'")
+        result = cur.fetchone()
+        if not result:
+            cur.execute(f"insert into ties values ('{id}', '{uid}', '{act}')")
+
+    connect.commit()
+    return redirect(url_for('user_info', uid=back))
+
+
+@app.route('/add-movie', methods=['post'])
+def add_movie():
+    id = session['id']
+    title = request.form.get('title')
+    director = request.form.get('director')
+    genre = request.form.get('genre')
+    rel_date = request.form.get('rel_date')
+    send = request.form.get('send')
+
+    if send:
+        cur.execute(f"select max(id) "
+                    f"from movies")
+        result = int(cur.fetchone()[0])
+        cur.execute(f"insert into movies values "
+                    f"('{result + 1}', '{title}', '{director}',"
+                    f" '{genre}', '{rel_date}')")
+        connect.commit()
+
+    return redirect(url_for('user_info', uid=id))
+
+
+@app.route('/delete-movie', methods=['post'])
+def delete_movie():
+    id = session['id']
+    mid = request.form.get('del_movie')
+    cur.execute(f"delete "
+                f"from reviews "
+                f"where mid = '{mid}'")
+    cur.execute(f"delete "
+                f"from movies "
+                f"where id = '{mid}'")
+    connect.commit()
+
+    flash(f"영화 id = '{mid}'와 리뷰가 삭제되었습니다.", category='success')
+    return redirect(url_for('user_info', uid=id))
+
+
+@app.route('/random-movie', methods=['post'])
+def random_movie():
+    genre = request.form.get('genre')
+    cur.execute(f"select id "
+                f"from movies "
+                f"where genre = '{genre}'")
+    result = cur.fetchall()
+
+    if result:
+        result = choice(result)
+        flash("이 영화는 어떤가요?", category='success')
+        return redirect(url_for('movie_info', mid=result[0]))
+    else:
+        flash(f"죄송합니다. '{genre}' 장르의 영화가 없습니다.", category='error')
+        return redirect(url_for('main_page'))
 
 
 if __name__ == '__main__':
