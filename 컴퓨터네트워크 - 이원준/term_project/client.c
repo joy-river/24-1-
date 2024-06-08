a25d1db9031c40ff08e5043492fe5889d8079557
harigaze <dlrkd1122@gmail.com>
2024-06-09 03:32:21 +0900

    Update

--- 컴퓨터네트워크 - 이원준/term_project/client.c
+++ 컴퓨터네트워크 - 이원준/term_project/client.c
@@ -6,17 +6,29 @@
 
 #define BUFFER_SIZE 1024
 
-void send_command(int sock, const char *input) {
+int send_command(int sock, const char *input) {
     char buffer[BUFFER_SIZE];
     send(sock, input, strlen(input), 0);
     int bytes_read = recv(sock, buffer, BUFFER_SIZE - 1, 0);
     buffer[bytes_read] = '\0';
+    
+    if (bytes_read <= 0) {
+        printf("서버와의 연결이 끊겼습니다.\n");
+        return -1;
+    }
+
+    if (strncmp(buffer, "-ERR", 4) == 0){
+        printf("%s\n서버 에러 발생, 클라이언트를 종료합니다.\n", buffer);
+        return -1;
+    }
+
     printf("%s\n", buffer);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <ip> <port>\n", argv[0]);
+        printf("./client <ip주소> <포트번호>로 입력해주세요. \n");
         return -1;
     }
 
@@ -27,7 +39,7 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in serv_addr;
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
+        perror("소켓 설정 에러");
         return -1;
     }
 
@@ -35,27 +47,29 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(port);
 
     if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
+        perror("유효하지 않은 주소");
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
+        perror("서버와 연결하지 못했습니다");
         return -1;
     }
+    else
+        printf("\n서버와 연결되었습니다.\n");
 
     char input[BUFFER_SIZE];
 
+
     while (1) {
+        printf("입력 대기중...\n");
         if (fgets(input, BUFFER_SIZE, stdin) != NULL) {
-            printf("%s", input);
-            if (strcmp(input, "EXIT") == 0){
-                printf("Goodbye!");
+            if (strncmp(input, "EXIT", 4) == 0){
+                printf("Goodbye.");
                 break;
             }
-            send_command(sock, input);
-        } else {
-            printf("Error reading input.\n");
+            if (send_command(sock, input) < 0)
+                break;
         }
     }
 
--- 컴퓨터네트워크 - 이원준/term_project/server.c
+++ 컴퓨터네트워크 - 이원준/term_project/server.c
@@ -5,104 +5,136 @@
 #include <arpa/inet.h>
 #include <sys/select.h>
 #include <errno.h>
+#include <signal.h>
 
-#define BUFFER_SIZE 1024
+#define STRING_SIZE 1024
 #define MAX_KEY 10
+#define MAX_CLIENT 10
 
 typedef struct {
     char key[1024];
     char value[1024];
-} KeyValue;
+} Key_Value;
 
-KeyValue store[MAX_KEY];
-int store_size = 0;
+Key_Value store[MAX_KEY];
+int len_store = 0;
+
+volatile sig_atomic_t stop = 0;
+
+void handle_sigint(int sig) {
+    (void)sig;
+    stop = 1;
+}
 
 void server_func(int client_socket, char *input) {
-    char response[BUFFER_SIZE] = {0};
+    char buffer[STRING_SIZE] = {0};
     char cmd[10], key[1024], value[1024];
-    sscanf(input, "%s %s %s", cmd, key, value);
+    int parsed_items = sscanf(input, "%s %s %s", cmd, key, value);
 
-    printf(input);
-
-    if (strcmp(cmd, "get") == 0 || strcmp(cmd, "GET") == 0) {
-        int found = 0;
-        for (int i = 0; i < store_size; i++) {
-            if (strcmp(store[i].key, key) == 0) {
-                strcpy(response, store[i].value);
-                found = 1;
-                break;
+    if (parsed_items < 1) {
+        strcpy(buffer, "-ERR: 유효하지 않은 입력입니다.");
+    } else if (strcmp(cmd, "get") == 0 || strcmp(cmd, "GET") == 0) {
+        if (parsed_items < 2) {
+            strcpy(buffer, "-ERR: GET 명령에 Key가 입력되지 않았습니다.");
+        } else {
+            int found = 0;
+            for (int i = 0; i < len_store; i++) {
+                if (strcmp(store[i].key, key) == 0) {
+                    snprintf(buffer, sizeof(buffer), "$%s", store[i].value);
+                    found = 1;
+                    break;
+                }
+            }
+            if (!found) {
+                strcpy(buffer, "-ERR: 해당되는 Key가 없습니다.");
             }
         }
-        if (!found) {
-            strcpy(response, "NULL");
-        }
-    } else if (strcmp(cmd, "set") == 0 || strcmp(cmd, "SET") == 0) {
-        int found = 0;
-        for (int i = 0; i < store_size; i++) {
-            if (strcmp(store[i].key, key) == 0) {
-                strcpy(store[i].value, value);
-                found = 1;
-                break;
+    } 
+    else if (strcmp(cmd, "set") == 0 || strcmp(cmd, "SET") == 0) {
+        if (parsed_items < 3) {
+            strcpy(buffer, "-ERR: SET 명령에 Key 혹은 Value가 입력되지 않았습니다.");
+        } else {
+            int found = 0;
+            for (int i = 0; i < len_store; i++) {
+                if (strcmp(store[i].key, key) == 0) {
+                    strcpy(store[i].value, value);
+                    found = 1;
+                    break;
+                }
             }
+            if (!found) {
+                strcpy(store[len_store].key, key);
+                strcpy(store[len_store].value, value);
+                len_store++;
+            }
+             strcpy(buffer, "+OK");
         }
-        if (!found && store_size < MAX_KEY) {
-            strcpy(store[store_size].key, key);
-            strcpy(store[store_size].value, value);
-            store_size++;
-        }
-        strcpy(response, "OK");
+    }
+    else if (strcmp(cmd, "EXIT") == 0){
+        strcpy(buffer, "");
     }
     else {
-        strcpy(response, "ERROR: Invalid input");
+        strcpy(buffer, "-ERR: 유효하지 않은 입력입니다.");
     }
 
-    send(client_socket, response, strlen(response), 0);
+    send(client_socket, buffer, strlen(buffer), 0);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        printf("./server <포트번호> 로 입력해주세요\n");
+        return -1;
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = handle_sigint;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        perror("sigaction 오류");
+        return -1;
     }
 
     int port = atoi(argv[1]);
-    int server_fd, client_socket, max_sd, activity, new_socket, addrlen;
-    int client_sockets[MAX_KEY] = {0};
+    int server_socket, client_socket, max_sd, sd, activity, new_socket, addrlen;
+
+    int client_sockets[MAX_CLIENT] = {0};
     struct sockaddr_in address;
-    char buffer[BUFFER_SIZE];
+    char buffer[STRING_SIZE];
 
     fd_set readfds;
+    struct timeval timeout;
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("소켓 생성 실패");
+        return -1;
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    if (bind(server_socket, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("서버 바인드 실패");
+        close(server_socket);
+        return -1;
     }
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    if (listen(server_socket, 3) < 0) {
+        perror("Listen 실패");
+        close(server_socket);
+        return -1;
     }
 
     addrlen = sizeof(address);
-    printf("Listening on port %d\n", port);
+    printf("포트번호 %d 에서 listen 중입니다...\n", port);
 
-    while (1) {
+    while (!stop) {
         FD_ZERO(&readfds);
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
+        FD_SET(server_socket, &readfds);
+        max_sd = server_socket;
 
-        for (int i = 0; i < MAX_KEY; i++) {
+        for (int i = 0; i < MAX_CLIENT; i++) {
             client_socket = client_sockets[i];
             if (client_socket > 0) {
                 FD_SET(client_socket, &readfds);
@@ -112,46 +144,73 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+        timeout.tv_sec = 1;
+        
+        activity = select(max_sd + 1, &readfds, NULL, NULL, &timeout);
 
         if ((activity < 0) && (errno != EINTR)) {
-            perror("select error");
+            perror("Select 오류");
+            break;
         }
 
-        if (FD_ISSET(server_fd, &readfds)) {
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-                perror("accept error");
-                exit(EXIT_FAILURE);
+        if (stop) {
+            break;
+        }
+
+        if (FD_ISSET(server_socket, &readfds)) {
+            while ((new_socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+                if (errno == EINTR) {
+                    if (stop) break;
+                    continue;
+                } else {
+                    perror("accept 오류");
+                    return -1;
+                }
             }
 
-            printf("New connection, socket fd is %d, ip is : %s, port : %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+            if (stop) {
+                break;
+            }
 
-            for (int i = 0; i < MAX_KEY; i++) {
+            printf("새로운 클라이언트가 연결되었습니다.\n socket_fd : %d\n ip : %s\n port : %d\n", new_socket, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            for (int i = 0; i < MAX_CLIENT; i++) {
                 if (client_sockets[i] == 0) {
                     client_sockets[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n", i);
+                    printf("%d번 소켓에 클라이언트가 추가되었습니다.\n", i);
                     break;
                 }
             }
         }
 
-        for (int i = 0; i < MAX_KEY; i++) {
+        for (int i = 0; i < MAX_CLIENT; i++) {
             client_socket = client_sockets[i];
 
             if (FD_ISSET(client_socket, &readfds)) {
-                int valread;
-                if ((valread = read(client_socket, buffer, BUFFER_SIZE)) == 0) {
+                int client_act;
+                if ((client_act = read(client_socket, buffer, STRING_SIZE)) == 0) {
                     getpeername(client_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-                    printf("Host disconnected, ip %s, port %d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                    printf("%d번 소켓의 클라이언트가 연결 해제되었습니다.\n ip : %s\n port %d\n", i, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
                     close(client_socket);
                     client_sockets[i] = 0;
                 } else {
-                    buffer[valread] = '\0';
+                    printf("%d번 소켓의 클라이언트의 요청을 처리합니다.\n", i);
+                    buffer[client_act] = '\0';
                     server_func(client_socket, buffer);
                 }
             }
         }
     }
+
+    for (int i = 0; i < MAX_CLIENT; i++) {
+        sd = client_sockets[i];
+        if (sd > 0) {
+            server_func(sd, "EXIT");
+            close(sd);
+        }
+    }
+    close(server_socket);
+    printf("\n서버가 종료되었습니다.\n");
 
     return 0;
 }
